9b4217f861898395ce3edd83f857df5e1712b407
JiefzzLon <kimffy24@gmail.com>
2019-07-26 22:56:50 +0800

    as2

--- src/main/java/pro/jiefzz/ejoker/demo/simple/transfer/boot/over_javaqueue/TransferApp.java
+++ src/main/java/pro/jiefzz/ejoker/demo/simple/transfer/boot/over_javaqueue/TransferApp.java
@@ -7,10 +7,6 @@ import pro.jiefzz.ejoker.demo.simple.transfer.boot.TransferPrepare;
 public class TransferApp {
 
 	public static void main(String[] args) throws Exception {
-
-		System.out.println("123456 ... ");
-		SleepWrapper.sleep(5000l);
-		
 		pro.jiefzz.ejoker.demo.simple.transfer.boot.over_rmp.TransferApp.start(TransferPrepare.prepare(new EJokerBootstrap()));
 	}
 	
