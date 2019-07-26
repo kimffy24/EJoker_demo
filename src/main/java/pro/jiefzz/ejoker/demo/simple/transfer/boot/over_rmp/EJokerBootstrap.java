2329435c099fa8fd38a6855eb74fbf6c95f0e4af
JiefzzLon <kimffy24@gmail.com>
2019-07-26 15:41:48 +0800

    ab

--- src/main/java/pro/jiefzz/ejoker/demo/simple/transfer/boot/over_rmp/EJokerBootstrap.java
+++ src/main/java/pro/jiefzz/ejoker/demo/simple/transfer/boot/over_rmp/EJokerBootstrap.java
@@ -8,7 +8,7 @@ import com.jiefzz.ejoker_support.rocketmq.MQInstanceHelper;
 
 public class EJokerBootstrap extends pro.jiefzz.ejoker.demo.simple.transfer.boot.AbstractEJokerBootstrap {
 
-	public final static String NameServAddr = "test_rocketmq_2:9876;test_sit_1:9876";
+	public final static String NameServAddr = "test_sit_1:9876";
 	
 	public EJokerBootstrap() {
 		super();
